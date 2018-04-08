f38da8b5420783d2316fbb0958a45a465446f3ca
Nick Foden <nickfoden@gmail.com>
2018-04-08 12:55:06 -0400

    Figured out how to get the data back from api and added entity of album to search string

--- src/Actions/artistActions.js
+++ src/Actions/artistActions.js
@@ -1,7 +1,9 @@
 export const searchArtistAction = ARTIST_NAME => {
-  fetch(`https://itunes.apple.com/search?term=${ARTIST_NAME}`).then(
-    response => {
-      console.log(response);
-    }
-  );
+  fetch(`https://itunes.apple.com/search?term=${ARTIST_NAME}&entity=album`)
+    .then(data => {
+      return data.json();
+    })
+    .then(json => {
+      console.log(json);
+    });
 };
